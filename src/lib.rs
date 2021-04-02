b1825b92e222f381944ddf7e512ceba6874fe564
Gabriel Berto <gabrielberto@protonmail.com>
2021-04-02 20:24:39 -0300

    feat: handle brakpoint exception

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,24 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
+#![feature(abi_x86_interrupt)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 pub mod serial;
 pub mod vga_buffer;
+pub mod interrupts;
 use core::panic::PanicInfo;
+
+pub fn init() {
+    interrupts::init_idt();
+}
 
 /// Entry point for `cargo test`
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
